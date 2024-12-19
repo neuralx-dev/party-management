8697df47a917cd78d1e0a8763aec17d26fca8e54
drgeek <mngmnt.hasannejad@gmail.com>
2024-12-19 23:47:16 +0330

    setting

--- check_be/settings.py
+++ check_be/settings.py
@@ -120,11 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Change to a different directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Media files
 MEDIA_URL = '/media/'
